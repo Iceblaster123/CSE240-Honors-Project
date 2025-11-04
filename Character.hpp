753d5083fe2916c603618c00291e72dc37d57654
K Joshi <joshikoowum@gmail.com>
2025-11-03 17:08:01 -0700

    na

--- Character.hpp
+++ Character.hpp
@@ -16,7 +16,7 @@ class Character{
         int getDamage() {return this->damage;};
         virtual string getType() = 0;
         virtual void setDamage(int level) = 0;
-        virtual void setHealth(int level) = 0;
+        virtual void setHealth(int health) = 0;
     protected:
         string name;
         int health = 0;
